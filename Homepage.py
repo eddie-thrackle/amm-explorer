1a1f61b4830761034c660ee0dbb6ecebc96f74f1
eddie-thrackle <eddie@thrackle.io>
2023-01-24 22:40:15 -0600

    make it suck less

--- Homepage.py
+++ Homepage.py
@@ -5,34 +5,6 @@ from sympy import latex
 st.set_page_config(layout="wide", page_title="TBC to AMM")
 st.title("TBC to AMM Explorer")
 
-st.markdown('''
-Token bonding curves and AMMs,
-Two sides of the same coin, it seems.
-One a mechanism for price discovery,
-The other for liquidity, it beams.
-
-Token bonding curves, a new way to raise,
-A method for creating value,
-It aligns incentives, and allows for growth,
-A path for projects to pursue.
-
-Automated market makers, on the other hand,
-Provide liquidity for all to see,
-They allow for easy buying and selling,
-And make the markets more efficient and free.
-
-Together, they form a powerful pair,
-A duality that's hard to ignore,
-One for creating value, the other for trading,
-A symbiotic relationship, evermore.
-
-Token bonding curves and AMMs,
-Two sides of the blockchain coin,
-Both vital for the growth and progress,
-Of the decentralized economy, we join.
-
-Written by [ChatGPT](https://chat.openai.com/chat) (01/24/2023).
-''')
 
 st.text('What comes first, AMM balances or TBC segments? A tale as old as DeFi.')
 st.text('''
@@ -129,3 +101,32 @@ def main():
 
 
 main()
+
+st.markdown("""
+Token bonding curves and AMMs,<br/>
+Two sides of the same coin, it seems.
+One a mechanism for price discovery,
+The other for liquidity, it beams.
+
+Token bonding curves, a new way to raise,
+A method for creating value,
+It aligns incentives, and allows for growth,
+A path for projects to pursue.
+
+Automated market makers, on the other hand,
+Provide liquidity for all to see,
+They allow for easy buying and selling,
+And make the markets more efficient and free.
+
+Together, they form a powerful pair,
+A duality that's hard to ignore,
+One for creating value, the other for trading,
+A symbiotic relationship, evermore.
+
+Token bonding curves and AMMs,
+Two sides of the blockchain coin,
+Both vital for the growth and progress,
+Of the decentralized economy, we join.
+
+ - Written by [ChatGPT](https://chat.openai.com/chat) (01/24/2023).
+""")
