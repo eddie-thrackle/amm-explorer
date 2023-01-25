2d70b2c5f5a450a190e664ff7900af607e655ea2
eddie-thrackle <eddie@thrackle.io>
2023-01-24 23:29:31 -0600

    the return of chatGPT

--- Homepage.py
+++ Homepage.py
@@ -100,31 +100,36 @@ def main():
 
 main()
 
-# st.markdown("""
-# Token bonding curves and AMMs,<br/>
-# Two sides of the same coin, it seems.
-# One a mechanism for price discovery,
-# The other for liquidity, it beams.
+st.markdown("""
 
-# Token bonding curves, a new way to raise,
-# A method for creating value,
-# It aligns incentives, and allows for growth,
-# A path for projects to pursue.
 
-# Automated market makers, on the other hand,
-# Provide liquidity for all to see,
-# They allow for easy buying and selling,
-# And make the markets more efficient and free.
 
-# Together, they form a powerful pair,
-# A duality that's hard to ignore,
-# One for creating value, the other for trading,
-# A symbiotic relationship, evermore.
 
-# Token bonding curves and AMMs,
-# Two sides of the blockchain coin,
-# Both vital for the growth and progress,
-# Of the decentralized economy, we join.
 
-#  - Written by [ChatGPT](https://chat.openai.com/chat) (01/24/2023).
-# """)
+Token bonding curves and AMMs,<br/>
+Two sides of the same coin, it seems.
+One a mechanism for price discovery,
+The other for liquidity, it beams.
+
+Token bonding curves, a new way to raise,
+A method for creating value,
+It aligns incentives, and allows for growth,
+A path for projects to pursue.
+
+Automated market makers, on the other hand,
+Provide liquidity for all to see,
+They allow for easy buying and selling,
+And make the markets more efficient and free.
+
+Together, they form a powerful pair,
+A duality that's hard to ignore,
+One for creating value, the other for trading,
+A symbiotic relationship, evermore.
+
+Token bonding curves and AMMs,
+Two sides of the blockchain coin,
+Both vital for the growth and progress,
+Of the decentralized economy, we join.
+
+ - Written by [ChatGPT](https://chat.openai.com/chat) (01/24/2023).
+""")
